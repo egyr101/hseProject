ca22f6e13221f914d9b72d86736fa5e10906e9c4
egyr101 <164477129+egyr101@users.noreply.github.com>
2025-12-20 16:54:19 +0500

    Update TaskOfStrings.cs

--- laba56/TaskOfStrings.cs
+++ laba56/TaskOfStrings.cs
@@ -1,34 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace laba56
 {
+    // Класс для работы со строками
     public class TaskOfStrings
     {
+        // Поле класса
         private string stringOfUser;
+
+        // Конструктор полностью создает объект
         public TaskOfStrings(string stringOfUser)
         {
             this.stringOfUser = stringOfUser;
         }
 
+        // Возвращает поле строки
         public string GetString()
         {
             return stringOfUser;
         }
 
+        // Смена первого и последнего слова в строке
         public bool Task()
         {
+            // Разделяем строку на массив слов
             string[] stringArray = ToStringArray();
             if (stringArray != null) 
             {
+                // Смена слов с помощью промежуточной переменной
                 string tmp = char.ToLower(stringArray[0][0]) + stringArray[0].Substring(1) + stringArray[stringArray.Length - 1][stringArray[stringArray.Length - 1].Length-1];
                 stringArray[stringArray.Length - 1] = char.ToUpper(stringArray[stringArray.Length - 1][0]) + stringArray[stringArray.Length - 1].Substring(1, stringArray[stringArray.Length - 1].Length - 2);
                 stringArray[0] = stringArray[stringArray.Length - 1];
                 stringArray[stringArray.Length - 1] = tmp;
-
+                
                 stringOfUser = String.Join(" ", stringArray);
 
                 return true;
@@ -37,6 +41,7 @@ namespace laba56
             
         }
 
+        // Разделение строки на массив слов
         private string[] ToStringArray()
         {
             if (stringOfUser.IndexOf(" ") == -1)
@@ -47,3 +52,4 @@ namespace laba56
         }
     }
 }
+
