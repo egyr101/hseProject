abb58388834e63384e4ce3c91ee7001fbec4773d
egyr101 <164477129+egyr101@users.noreply.github.com>
2025-12-20 16:54:50 +0500

    Update CustomErrors.cs

--- laba56/CustomErrors.cs
+++ laba56/CustomErrors.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace laba56
 {
@@ -27,4 +23,5 @@ namespace laba56
         public const string errorMessageStringHasOneWord = "Операция невозможна, так как в строке только одно слово!";
     }
 }
+
 
